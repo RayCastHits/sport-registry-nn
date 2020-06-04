00dcd942fe9896879065b6030c99f995d9c3b5f6
kiselev Vladimir <wsxxx2016@yandex.ru>
2020-06-05 00:55:42 +0300

    add cleaned method

---
+++ source/cleaning.py
@@ -0,0 +1,8 @@
+def get_clean_primary_data(data):
+    result = []
+    return result
+
+
+def get_clean_medical_data(data):
+    result = []
+    return result
--- source/data.py
+++ source/data.py
@@ -1,4 +1,7 @@
 from .services import get_json
-from ..settings import SPORTSMAN_LIST_URL
+from .cleaning import get_clean_primary_data, get_clean_medical_data
+from .settings import SPORTSMAN_LIST_URL
 
 DATA = get_json(SPORTSMAN_LIST_URL)
+CLEAN_PRIMARY_DATA = get_clean_primary_data(DATA)
+CLEAN_MEDICAL_DATA = get_clean_medical_data(DATA)
